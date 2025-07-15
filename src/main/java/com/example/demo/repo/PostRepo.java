68afe293767c336528929812f2e0866b4d08a87e
nithyasri12345 <ns4909379@gmail.com>
2025-07-15 12:38:22 +0530

    Update PostRepo

--- src/main/java/com/example/demo/repo/PostRepo.java
+++ src/main/java/com/example/demo/repo/PostRepo.java
@@ -1,7 +1,17 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Post;
 
-public interface PostRepo extends JpaRepository<Post, Long> {}
+public interface PostRepo extends JpaRepository<Post, Long> {
+	
+	    List<Post> findAllByOrderByIdDesc(); 
+	    List<Post> findByTitleContainingIgnoreCaseOrderByIdDesc(String title);
+
+	
+
+}
+
