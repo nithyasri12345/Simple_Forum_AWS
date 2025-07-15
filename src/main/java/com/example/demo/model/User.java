f0675068ca72bae9d3dd4a6e565fb568026adfe2
nithyasri12345 <ns4909379@gmail.com>
2025-07-15 15:16:43 +0530

    Update User

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
